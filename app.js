9b0fe552321b3c0a7c8774e9943fc6d111d03644
Andy Albrecht <aalbrecht33733@gmail.com>
2022-10-21 17:02:31 -0400

    Button Impossible, silly event listener exercise

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector('button');
 
 btn.addEventListener('mouseover', () => {
-    console.log("YOU MOUSED OVER ME!!")
-    btn.style.left = '200px';
-    btn.style.top = '100px';
+    console.log("YOU MOUSED OVER ME!!");
+    const height = Math.floor(Math.random() * window.innerHeight);
+    const width = Math.floor(Math.random() * innerWidth);
+
+    btn.style.left = `${width}px`
+    btn.style.top = `${height}px`
+})
+
+btn.addEventListener('click', function() {
+    btn.innerText = "YOU GOT ME!";
+    document.body.style.backgroundColor = "green";
 })
